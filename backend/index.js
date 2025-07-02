c25aaede67b9e55c38db1fce2d35183cffe34043
Suyog <mulukhsuyog@gmail.com>
2025-07-02 11:41:47 +0530

    production ready app

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const userRoutes = require('./user.routes');
-const imageRoutes = require('./image.routes');
-const enhanceRoutes = require('./enhance.routes');
+const userRoutes = require('./routes/user.routes');
+const imageRoutes = require('./routes/image.routes');
+const enhanceRoutes = require('./routes/enhance.routes');
+const path = require('path');
 
 const router = express.Router();
+
+const _dirname = path.resolve();
 
 // Register all routes
 router.use('/users', userRoutes);
 router.use('/image', imageRoutes);
 router.use('/image/enhance', enhanceRoutes);
+
+router.use(express.static(path.join(_dirname, "/frontend/dist")));
+router.get('*', (_, res) => {
+    res.sendFile(path.resolve(_dirname, "./frontend/dist/index.html"));
+});
 
 module.exports = router;
